f1d69df938151aa7f344bd3367e945f541ce3d5b
Dave <dave.chia88@gmail.com>
2023-10-25 03:00:31 +0800

    Refactored routes to move product logic into controller file

---
+++ controllers/taskControllers.js
@@ -0,0 +1,90 @@
+const mysqlDb = require("../databases/mysqlDb");
+const { validationResult } = require("express-validator");
+
+const formatValidationErrors = (validationErrors) => {
+  let output = {};
+
+  validationErrors.forEach((error) => {
+    if (error.path in output) {
+      output[error.path].push(error.msg);
+    } else {
+      output[error.path] = [error.msg];
+    }
+  });
+
+  return output;
+};
+
+exports.getTasks = (req, res) => {
+  mysqlDb.query("SELECT * FROM tasks", (err, results) => {
+    if (err) {
+      res.status(500).json({ error: "Failed to fetch tasks" });
+    } else {
+      res.status(200).json({ data: results });
+    }
+  });
+};
+
+exports.createTask = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ errors: formatValidationErrors(errors.array()) });
+  }
+
+  const { name, description, completed } = req.body;
+  const newItem = { name, description, completed };
+
+  mysqlDb.query("INSERT INTO tasks SET ?", newItem, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: "Failed to create a task" });
+    } else {
+      res.status(201).json({ message: "task created successfully" });
+    }
+  });
+};
+
+exports.getTask = (req, res) => {
+  const itemId = req.params.id;
+  mysqlDb.query(
+    "SELECT * FROM tasks WHERE id = ? LIMIT 1",
+    [itemId],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ error: "Failed to fetch task" });
+      } else if (result.affectedRows === 0) {
+        res.status(404).json({ message: "Task not found" });
+      } else {
+        res.status(200).json({ data: result[0] });
+      }
+    }
+  );
+};
+
+exports.editTask = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ errors: formatValidationErrors(errors.array()) });
+  }
+
+  const itemId = req.params.id;
+  const updatedItem = req.body;
+  mysqlDb.query(
+    "UPDATE tasks SET ? WHERE id = ?",
+    [updatedItem, itemId],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ error: err });
+      } else if (result.affectedRows === 0) {
+        res.status(404).json({ message: "Task not found" });
+      } else {
+        res.status(200).json({ message: "Task updated successfully" });
+      }
+    }
+  );
+};
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,22 +1,7 @@
 const express = require("express");
-const mysqlDb = require("../databases/mysqlDb");
 const router = express.Router();
-
-const { check, validationResult } = require("express-validator");
-
-const formatValidationErrors = (validationErrors) => {
-  let output = {};
-
-  validationErrors.forEach((error) => {
-    if (error.path in output) {
-      output[error.path].push(error.msg);
-    } else {
-      output[error.path] = [error.msg];
-    }
-  });
-
-  return output;
-};
+const { check } = require("express-validator");
+const taskController = require("../controllers/taskControllers");
 
 const taskValidationRules = [
   check("name")
@@ -34,78 +19,9 @@ const taskValidationRules = [
     .withMessage("This field must be at a boolean character."),
 ];
 
-router.post("/", taskValidationRules, (req, res) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({ errors: formatValidationErrors(errors.array()) });
-  }
-
-  const { name, description, completed } = req.body;
-  const newItem = { name, description, completed };
-
-  mysqlDb.query("INSERT INTO tasks SET ?", newItem, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: "Failed to create a task" });
-    } else {
-      res.status(201).json({ message: "task created successfully" });
-    }
-  });
-});
-
-router.get("/", (req, res) => {
-  mysqlDb.query("SELECT * FROM tasks", (err, results) => {
-    if (err) {
-      res.status(500).json({ error: "Failed to fetch tasks" });
-    } else {
-      res.status(200).json({ data: results });
-    }
-  });
-});
-
-router.get("/:id", (req, res) => {
-  const itemId = req.params.id;
-  mysqlDb.query(
-    "SELECT * FROM tasks WHERE id = ? LIMIT 1",
-    [itemId],
-    (err, result) => {
-      if (err) {
-        res.status(500).json({ error: "Failed to fetch task" });
-      } else if (result.affectedRows === 0) {
-        res.status(404).json({ message: "Task not found" });
-      } else {
-        res.status(200).json({ data: result[0] });
-      }
-    }
-  );
-});
-
-router.put("/:id", taskValidationRules, (req, res) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({ errors: formatValidationErrors(errors.array()) });
-  }
-
-  const itemId = req.params.id;
-  const updatedItem = req.body;
-  mysqlDb.query(
-    "UPDATE tasks SET ? WHERE id = ?",
-    [updatedItem, itemId],
-    (err, result) => {
-      if (err) {
-        res.status(500).json({ error: err });
-      } else if (result.affectedRows === 0) {
-        res.status(404).json({ message: "Task not found" });
-      } else {
-        res.status(200).json({ message: "Task updated successfully" });
-      }
-    }
-  );
-});
+router.post("/", taskValidationRules, taskController.createTask);
+router.get("/:id", taskController.getTask);
+router.get("/", taskController.getTasks);
+router.put("/:id", taskController.editTask);
 
 module.exports = router;
