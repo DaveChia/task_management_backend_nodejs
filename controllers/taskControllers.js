16ac4c0babc74eaaa0e4f89df70a70030620628c
Dave <dave.chia88@gmail.com>
2023-10-25 13:55:28 +0800

    Improved query to store and update a task

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -27,13 +27,17 @@ exports.createTask = (req, res) => {
   const { name, description, completed } = req.body;
   const newItem = { name, description, completed };
 
-  mysqlDb.query("INSERT INTO tasks SET ?", newItem, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: "Failed to retreive tasks" });
-    } else {
-      res.status(201).json({ message: "task created successfully" });
+  mysqlDb.query(
+    "INSERT INTO tasks SET name = ?, description = ?, completed = ?, created_at = NOW(), updated_at = NOW()",
+    [newItem.name, newItem.description, newItem.completed],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ error: "Failed to retreive tasks" });
+      } else {
+        res.status(201).json({ message: "task created successfully" });
+      }
     }
-  });
+  );
 };
 
 exports.getTask = (req, res) => {
@@ -63,10 +67,13 @@ exports.editTask = (req, res) => {
   }
 
   const itemId = req.params.id;
-  const updatedItem = req.body;
+
+  const { name, description, completed } = req.body;
+  const updatedItem = { name, description, completed };
+
   mysqlDb.query(
-    "UPDATE tasks SET ? WHERE id = ?",
-    [updatedItem, itemId],
+    "UPDATE tasks SET name = ?, description = ?, completed = ?, updated_at = NOW() WHERE id = ?",
+    [updatedItem.name, updatedItem.description, updatedItem.completed, itemId],
     (err, result) => {
       if (err) {
         res.status(500).json({ error: err });
